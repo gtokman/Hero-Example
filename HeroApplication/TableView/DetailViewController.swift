c056e50a98f80774154f626a0d8e1cd71e13a2c0
Gary Guerman Tokman <gtokman@fullsail.edu>
2017-11-01 10:35:10 -0400

    Finished project with comments

--- HeroApplication/TableView/DetailViewController.swift
+++ HeroApplication/TableView/DetailViewController.swift
@@ -14,7 +14,7 @@ class DetailViewController: UIViewController {
     // MARK: - Outlets
     
     @IBOutlet var detailImageView: UIImageView!
-
+    
     var unsplashImage: Image?
     
     override func viewDidLoad() {
@@ -23,11 +23,36 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view.
         if let image = unsplashImage {
             detailImageView.image = UIImage(named: image.id)
+            // Hero ID for shared UIElement between source/destination
+            detailImageView.heroID = image.id
         }
     }
     
     // MARK: - Actions
     
     @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
+        // Get translation
+        let translation = sender.translation(in: nil)
+        let progress = translation.y / 2 / view.bounds.height
+        switch sender.state {
+        case .began:
+            // Start the dismiss of View Controller on Pan
+            hero_dismissViewController()
+        case .changed:
+            // Update pan progress
+            Hero.shared.update(progress)
+            
+            // Add additional animations with modifiers
+            let currentPos = CGPoint(x: translation.x + detailImageView.center.x, y: translation.y + detailImageView.center.y)
+            Hero.shared.apply(modifiers: [.position(currentPos)], to: detailImageView)
+        default:
+            // Cancel or finish transition
+            if progress + sender.velocity(in: nil).y / view.bounds.height > 0.2 {
+                Hero.shared.finish()
+            } else {
+                Hero.shared.cancel()
+            }
+            
+        }
     }
 }
--- HeroApplication/TableView/ImageCell.swift
+++ HeroApplication/TableView/ImageCell.swift
@@ -18,6 +18,8 @@ class ImageCell: UITableViewCell {
     var unsplashImage: Image? {
         didSet {
             cellImageView.image = UIImage(named: unsplashImage!.id)
+            // Hero ID for shared UIElement between source/destination
+            cellImageView.heroID = unsplashImage!.id
         }
     }
 
